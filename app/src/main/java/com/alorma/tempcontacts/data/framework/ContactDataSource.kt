ec5d04171a8bd173f7724d74f7dd1d53902ee743
Bernat Borrás Paronella <bernatbor15@gmail.com>
2018-06-30 11:34:25 +0200

    Use ActionLiveData to load contact

--- app/src/main/java/com/alorma/tempcontacts/data/framework/ContactDataSource.kt
+++ app/src/main/java/com/alorma/tempcontacts/data/framework/ContactDataSource.kt
@@ -5,29 +5,26 @@ import android.content.Context
 import android.net.Uri
 import android.provider.ContactsContract
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import com.alorma.tempcontacts.data.ActionLiveData
 import com.alorma.tempcontacts.domain.model.Contact
 import javax.inject.Inject
 
 class ContactDataSource @Inject constructor(private val context: Context) {
 
-    fun loadContact(contactUri: Uri): LiveData<Contact?> = MutableLiveData<Contact?>().apply {
-        postValue(getContact(contactUri))
+    fun loadContact(contactUri: Uri): LiveData<Contact?> = ActionLiveData {
+        context.contentResolver.query(contactUri, null, null, null, null)
+                .takeIf { it.moveToFirst() }?.let {
+                    val idIndex = it.getColumnIndex(ContactsContract.Contacts._ID)
+                    val nameIndex = it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+
+                    val id = it.getString(idIndex)
+                    val name = it.getString(nameIndex)
+
+                    it.close()
+
+                    Contact(id, name, 0)
+                }
     }
-
-    private fun getContact(contactUri: Uri): Contact? =
-            context.contentResolver.query(contactUri, null, null, null, null)
-                    .takeIf { it.moveToFirst() }?.let {
-                        val idIndex = it.getColumnIndex(ContactsContract.Contacts._ID)
-                        val nameIndex = it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
-
-                        val id = it.getString(idIndex)
-                        val name = it.getString(nameIndex)
-
-                        it.close()
-
-                        Contact(id, name, 0)
-                    }
 
     fun delete(androidId: String) {
         val uri = ContentUris.appendId(ContactsContract.Contacts.CONTENT_URI.buildUpon(),
